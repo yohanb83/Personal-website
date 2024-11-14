d9a50cd3c2ad7436259016987bd7ca076911365d
yohanb83 <bohecyohan@gmail.com>
2024-11-14 10:06:17 +0100

    added main page elements

---
+++ vue-project/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="content-div">
+    <div id="title-btn-div">
+      <div id="title-div">
+        <h1 id="head-title">HEAD TITLE</h1>
+        <h2 id="subtitle">Subtitle</h2>
+      </div>
+      <div id="btn-div">
+        <a href="">About Me</a>
+        <a href="">Portfolio</a>
+        <a href="">Resume</a>        
+      </div>
+    </div>
+    <div id="profile-div">
+      <div id="pict-container">
+        <img src="./assets/images/default_profile.png" width="300" height="300">
+      </div>
+      <div id="id-div">
+        <h1 id="name">Yohan BOHEC</h1>
+        <h2 id="pers-desc">HMI Student / Freelance UX Designer</h2>
+        <button>Contact Me</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
